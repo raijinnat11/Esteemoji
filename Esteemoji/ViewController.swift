593787abb659e5930ef92a160677b09fc1d6b254
Raitah Jinnat <jinnatr@bxscience.edu>
2019-07-26 11:44:24 -0400

    Middle of Iteration 1

--- Esteemoji/ViewController.swift
+++ Esteemoji/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+   
+   
+   
+    @IBAction func showMessage(sender: UIButton) {
+        // message needs to show up with following details: title, random message, button to close out!!
+        let alertControl = UIAlertController(title: "Cheer up", message: "The person reading this is cute", preferredStyle: UIAlertController.Style.alert)
+        alertControl.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertControl, animated: true, completion: nil)
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
